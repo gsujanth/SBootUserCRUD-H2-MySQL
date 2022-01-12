eb6611d6504379dd2f005f5b5e4d86ce73835611
gsujanth <princesujanth@gmail.com>
2022-01-11 22:06:03 -0500

    remove un-necessary @Component annotation

--- src/main/java/com/training/springbootdemo/security/UserCrudAuthenticationEntryPoint.java
+++ src/main/java/com/training/springbootdemo/security/UserCrudAuthenticationEntryPoint.java
@@ -4,14 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 
-@Component
 public class UserCrudAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
